f318ad9febe9cd0340a455a92bed9fdc9f5f9fd9
David Beltran Gomez <estedavid0104@gmail.com>
2025-10-12 19:00:15 -0500

    Se modificia el programa principal para el funcionamiento y que apenas inicie se levante la base de datos utilizando el script.

--- src/main/java/Main/Stella.java
+++ src/main/java/Main/Stella.java
@@ -5,6 +5,7 @@ import Application.services.DarAccesoService;
 import Infrastructure.repositories.UsuarioRepository;
 import Domain.repositoriesInterfaces.InterfazUsuarioRepository;
 import Infrastructure.controllers.LoginController;
+import Infrastructure.persistence.H2DataBaseInitializer; // <--- Importación necesaria
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,12 +16,18 @@ import javafx.stage.Stage;
 public class Stella extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+
+        // **********************************************
+        // 1. PASO CRÍTICO: INICIALIZAR LA BASE DE DATOS
+        H2DataBaseInitializer.initialize();
+        // **********************************************
+
         // Lo del back se llama una sola vez con repo y service
         InterfazUsuarioRepository repo = new UsuarioRepository();
         DarAccesoService service = new DarAccesoService(repo);
         AppServices.init(service);
 
-        // SE carga FXML e inyecta controladores cuando se necesite
+        // S carga FXML e inyecta controladores cuando se necesite
         FXMLLoader loader = new FXMLLoader(Stella.class.getResource("/views/hello-view.fxml"));
         loader.setControllerFactory(clazz -> {
             if (clazz == LoginController.class)
