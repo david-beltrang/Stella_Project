5b8f64cbd19a4fc257db5684a0e7fc9c19162ae2
dgior <giovannyduran2906@gmail.com>
2025-10-20 13:29:38 -0500

    Se corrige el controlador de la pantalla principal (PrincipalController) para incorporalo con el back, y se crean getter y setter en AppService para poder acceder al id del usuario el cual inicia la sesion

--- src/main/java/Application/config/AppServices.java
+++ src/main/java/Application/config/AppServices.java
@@ -1,11 +1,12 @@
 package Application.config;
 
+import Application.dtos.acceso.UsuarioResponse;
 import Application.services.DarAcceso.DarAccesoService;
 import Application.services.DarAcceso.LoginService;
 
 public final class AppServices {
     private static LoginService service;
-
+    private static UsuarioResponse usuarioActual;
     private AppServices() {}
 
     // Esta función es para inicializar el servicio de acceso a la base de datos
@@ -14,5 +15,17 @@ public final class AppServices {
     }
     public static LoginService service() {
         return service;
+    }
+
+    public static UsuarioResponse getUsuarioActual() {
+        return usuarioActual;
+    }
+
+    //Gets y sets para acceder a la sesion del usuario
+    public static void setUsuarioActual(UsuarioResponse usuario) {
+        AppServices.usuarioActual = usuario;
+    }
+    public static void cerrarSesion(){
+        usuarioActual = null;
     }
 }
